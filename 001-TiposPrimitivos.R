0b4560e5d225046b04d3d182c02bea71fc4444b7
Jvmachado01 <joaommachado89@outlook.com>
2024-05-22 18:46:14 -0300

    tipos de dados

---
+++ 001-TiposPrimitivos.R
@@ -0,0 +1,9 @@
+
+# Operadores aritméticos
+2 + 2
+2 - 1
+2 * 5
+10 / 2
+2 ^ 3
+
+
---
+++ 002-OperadoresDeComparacao.R
@@ -0,0 +1,11 @@
+numero01 <- 10
+numero02 <- 20
+numero03 <- 30
+numero04 <- 40
+numeroIgual <- 40
+
+print(numero04 == numeroIgual)
+print(numero01 != numero02)
+print(numero01 < numero02)
+print(numero04 <= numeroIgual)
+print(numero02 > numero04)
